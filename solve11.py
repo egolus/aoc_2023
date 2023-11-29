3b5ba1ae6552859fe291b15b9be9de991b7fc8dd
Tobias <tobias_maier99@gmx.de>
2023-11-29 20:27:41 +0100

    prepared files

---
+++ solve11.py
@@ -0,0 +1,42 @@
+from aocd import submit, get_data
+
+
+def main():
+    day = 11
+    year = 2023
+    data = get_data(day=day, year=year)
+
+    test_data_a = {
+    }
+    test_data_b = {
+    }
+
+    for i, (test, true) in enumerate(test_data_a.items()):
+        result = solve_a(test)
+        print(f"result {i}: {result}\n")
+        assert result == true, f"{result} != {true}"
+
+    result_a = solve_a(data)
+    print(f"result a: {result_a}\n")
+    submit(result_a, part="a", day=day, year=year)
+
+    for i, (test, true) in enumerate(test_data_b.items()):
+        result = solve_b(test)
+        print(f"result {i}: {result}\n")
+        assert result == true, f"{result} != {true}"
+
+    result_b = solve_b(data)
+    print(f"result b: {result_b}\n")
+    submit(result_b, part="b", day=day, year=year)
+
+
+def solve_a(data):
+    pass
+
+
+def solve_b(data):
+    pass
+
+
+if __name__ == "__main__":
+    main()
