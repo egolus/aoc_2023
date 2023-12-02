6a1d9e260d68f98027380ecc3ef96f39b9b72145
Tobias <tobias_maier99@gmx.de>
2023-12-02 06:31:07 +0100

    day 2

--- solve02.py
+++ solve02.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from operator import mul
 from aocd import submit, get_data
 
 
@@ -7,8 +9,18 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
+            """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""": 8,
     }
     test_data_b = {
+            """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""": 2286,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -31,11 +43,46 @@ def main():
 
 
 def solve_a(data):
-    pass
+    possible = []
+    for line in data.splitlines():
+        line = line.strip()
+        cubes = {}
+        game, plays = line.split(":")
+        game = game.split(" ")[1]
+        for x in plays.split(";"):
+            for p in x.split(","):
+                p = p.strip().split(" ")
+                if p[1] in cubes:
+                    if int(p[0]) > cubes[p[1]]:
+                        cubes[p[1]] = int(p[0])
+                else:
+                    cubes[p[1]] = int(p[0])
+        if (("red" in cubes and cubes["red"] > 12) or
+                ("green" in cubes and cubes["green"] > 13) or
+                ("blue" in cubes and cubes["blue"] > 14)):
+            continue
+        else:
+            possible.append(int(game))
+    return sum(possible)
 
 
 def solve_b(data):
-    pass
+    possible = []
+    for line in data.splitlines():
+        line = line.strip()
+        cubes = {}
+        game, plays = line.split(":")
+        game = game.split(" ")[1]
+        for x in plays.split(";"):
+            for p in x.split(","):
+                p = p.strip().split(" ")
+                if p[1] in cubes:
+                    if int(p[0]) > cubes[p[1]]:
+                        cubes[p[1]] = int(p[0])
+                else:
+                    cubes[p[1]] = int(p[0])
+        possible.append(reduce(mul, cubes.values(), 1))
+    return sum(possible)
 
 
 if __name__ == "__main__":
