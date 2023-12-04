f6732ef9d0b9c85e11a1434edbefbc4e181ba2af
Tobias <tobias_maier99@gmx.de>
2023-12-04 06:39:31 +0100

    day 4

--- solve04.py
+++ solve04.py
@@ -1,4 +1,5 @@
 from aocd import submit, get_data
+from collections import defaultdict
 
 
 def main():
@@ -7,8 +8,20 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
+            """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11 """: 13,
     }
     test_data_b = {
+            """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""": 30,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -31,11 +44,44 @@ def main():
 
 
 def solve_a(data):
-    pass
+    sol = 0
+
+    for line in data.splitlines():
+        points = 0
+        line = line.strip()
+        cid, other = line.split(":")
+        a, b = other.split("|")
+        wins = [int(x) for x in a.split()]
+        mine = [int(x) for x in b.split()]
+        for x in mine:
+            if x in wins:
+                if points == 0:
+                    points = 1
+                else:
+                    points *= 2
+        sol += points
+    return sol
 
 
 def solve_b(data):
-    pass
+    i = 0
+    lines = data.splitlines()
+    copies = defaultdict(int)
+
+    for line in data.splitlines():
+        points = 0
+        line = line.strip()
+        cid, other = line.split(":")
+        a, b = other.split("|")
+        wins = [int(x) for x in a.split()]
+        mine = [int(x) for x in b.split()]
+        for x in mine:
+            if x in wins:
+                points += 1
+        for j in range(i+1, i+points+1):
+            copies[j] += copies[i] + 1
+        i += 1
+    return sum(copies.values()) + len(lines)
 
 
 if __name__ == "__main__":
