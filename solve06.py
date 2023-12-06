881f5f846f223048d538a907657d6fb8f5ced7fc
Tobias <tobias_maier99@gmx.de>
2023-12-06 07:05:09 +0100

    day 6

--- solve06.py
+++ solve06.py
@@ -7,8 +7,12 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
+            """Time:      7  15   30
+Distance:  9  40  200""": 288,
     }
     test_data_b = {
+            """Time:      7  15   30
+Distance:  9  40  200""": 71503,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -31,11 +35,26 @@ def main():
 
 
 def solve_a(data):
-    pass
+    res = 1
+    tl, dl = [ln.split(":")[1] for ln in data.splitlines()]
+    times = [int(x) for x in tl.split()]
+    dists = [int(x) for x in dl.split()]
+
+    for r in range(len(times)):
+        time, dist = times[r], dists[r]
+        forces = [(time - i) * i for i in range(time)]
+        wins = [x for x in forces if x > dist]
+        res *= len(wins)
+    return res
 
 
 def solve_b(data):
-    pass
+    tl, dl = [ln.split(":")[1] for ln in data.splitlines()]
+    time = int(tl.replace(" ", ""))
+    dist = int(dl.replace(" ", ""))
+    forces = [(time - i) * i for i in range(time)]
+    wins = [x for x in forces if x > dist]
+    return len(wins)
 
 
 if __name__ == "__main__":
