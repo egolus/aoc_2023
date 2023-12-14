5375279fbd5457158f2d5af9d2ef44ebcc007087
Tobias <tobias_maier99@gmx.de>
2023-12-14 07:27:15 +0100

    day 14

--- solve14.py
+++ solve14.py
@@ -7,8 +7,28 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
+            """O....#....
+            O.OO#....#
+            .....##...
+            OO.#O....O
+            .O.....O#.
+            O.#..O.#.#
+            ..O..#O..O
+            .......O..
+            #....###..
+            #OO..#....""": 136,
     }
     test_data_b = {
+            """O....#....
+            O.OO#....#
+            .....##...
+            OO.#O....O
+            .O.....O#.
+            O.#..O.#.#
+            ..O..#O..O
+            .......O..
+            #....###..
+            #OO..#....""": 64,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -30,12 +50,117 @@ def main():
     submit(result_b, part="b", day=day, year=year)
 
 
+def printGrid(grid, maxY, maxX):
+    print("-"*maxX)
+    for y in range(maxY):
+        for x in range(maxX):
+            if (y, x) in grid:
+                print(grid[(y, x)], end="")
+            else:
+                print(" ", end="")
+        print()
+    print()
+
+
 def solve_a(data):
-    pass
+    ans = 0
+    grid = {}
+    for y, line in enumerate(data.splitlines()):
+        for x, c in enumerate(line.strip()):
+            if c in "O#":
+                grid[(y, x)] = c
+    maxY = y + 1
+    maxX = x + 1
+
+    # printGrid(grid, maxY, maxX)
+
+    for y in range(maxY):
+        for x in range(maxX):
+            if grid.get((y, x), "") == "O":
+                dy = y
+                for dy in range(y-1, -2, -1):
+                    if (dy, x) in grid:
+                        break
+                grid[(dy+1, x)] = grid.pop((y, x))
+    # printGrid(grid, maxY, maxX)
+
+    for y in range(maxY):
+        for x in range(maxX):
+            if grid.get((y, x), "") == "O":
+                ans += maxY - y
+    return ans
 
 
-def solve_b(data):
-    pass
+def solve_b(data, cycles=1_000_000_000):
+    ans = 0
+    grid = {}
+    for y, line in enumerate(data.splitlines()):
+        for x, c in enumerate(line.strip()):
+            if c in "O#":
+                grid[(y, x)] = c
+    maxY = y + 1
+    maxX = x + 1
+
+    answers = []
+    for i in range(1000):
+        # north
+        for y in range(maxY):
+            for x in range(maxX):
+                if grid.get((y, x), "") == "O":
+                    dy = y
+                    for dy in range(y-1, -2, -1):
+                        if (dy, x) in grid:
+                            break
+                    grid[(dy+1, x)] = grid.pop((y, x))
+        # printGrid(grid, maxY, maxX)
+
+        # west
+        for x in range(maxX):
+            for y in range(maxY):
+                if grid.get((y, x), "") == "O":
+                    dx = x
+                    for dx in range(x-1, -2, -1):
+                        if (y, dx) in grid:
+                            break
+                    grid[(y, dx+1)] = grid.pop((y, x))
+        # printGrid(grid, maxY, maxX)
+
+        # south
+        for y in range(maxY, -1, -1):
+            for x in range(maxX):
+                if grid.get((y, x), "") == "O":
+                    dy = y
+                    for dy in range(y+1, maxY+1):
+                        if (dy, x) in grid:
+                            break
+                    grid[(dy-1, x)] = grid.pop((y, x))
+        # printGrid(grid, maxY, maxX)
+
+        # east
+        for x in range(maxX, -1, -1):
+            for y in range(maxY):
+                if grid.get((y, x), "") == "O":
+                    dx = x
+                    for dx in range(x+1, maxX+1):
+                        if (y, dx) in grid:
+                            break
+                    grid[(y, dx-1)] = grid.pop((y, x))
+        # printGrid(grid, maxY, maxX)
+
+        ans = 0
+        for y in range(maxY):
+            for x in range(maxX):
+                if grid.get((y, x), "") == "O":
+                    ans += maxY - y
+        answers.append(ans)
+    for di in range(len(answers)-2, 0, -1):
+        if (answers[di] == answers[-1] and
+                answers[di-1] == answers[-2] and
+                answers[di-2] == answers[-3]):
+            cycleStop = di
+            break
+    rest = (cycles - i) % (i-cycleStop)
+    return answers[cycleStop + rest - 1]
 
 
 if __name__ == "__main__":
