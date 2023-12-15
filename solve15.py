4aefb82bf0a9cd3cfb231b1cc0d0a574affc4331
Tobias <tobias_maier99@gmx.de>
2023-12-15 06:48:46 +0100

    day 15

--- solve15.py
+++ solve15.py
@@ -1,4 +1,5 @@
 from aocd import submit, get_data
+from collections import defaultdict
 
 
 def main():
@@ -7,8 +8,11 @@ def main():
     data = get_data(day=day, year=year)
 
     test_data_a = {
+            "HASH": 52,
+            "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7": 1320,
     }
     test_data_b = {
+            "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7": 145,
     }
 
     for i, (test, true) in enumerate(test_data_a.items()):
@@ -30,12 +34,52 @@ def main():
     submit(result_b, part="b", day=day, year=year)
 
 
+class Hash():
+    current = 0
+
+    def __call__(self, s):
+        for c in s:
+            self.char(c)
+        return self.current
+
+    def char(self, c):
+        self.current += ord(c)
+
+        self.current *= 17
+        self.current = self.current % 256
+
+
 def solve_a(data):
-    pass
+    res = 0
+    data = data.replace("\n", "")
+    for p in data.strip().split(","):
+        h = Hash()
+        val = h(p)
+        res += val
+    return res
 
 
 def solve_b(data):
-    pass
+    ans = 0
+    hashmap = defaultdict(dict)
+    data = data.replace("\n", "")
+    for p in data.strip().split(","):
+        box = Hash()(p[-1] if "-" in p else p.split("=")[0])
+        if "-" in p:
+            for box in hashmap:
+                if p[:-1] in hashmap[box]:
+                    hashmap[box].pop(p[:-1])
+        elif "=" in p:
+            k, v = p.split("=")
+            hashmap[box][k] = v
+
+    for k, v in sorted(hashmap.items()):
+        box = k + 1
+        for i, l in enumerate(v.values()):
+            s = i + 1
+            l = int(l)
+            ans += box * s * l
+    return ans
 
 
 if __name__ == "__main__":
